130b00dba4fa20e0632811acb74637a80ed3c742
Tika299 <vukofa9120@gmail.com>
2025-04-03 22:23:50 +0700

    Update my-lavarel

---
+++ my-lavarel/example-app/resources/views/crud_user/list.blade.php
@@ -0,0 +1,54 @@
+@extends('dashboard')
+
+@section('content')
+<div class="root">
+    <div class="background-list">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>UserName</th>
+                    <th>Email</th>
+                    <th>Like</th>
+                    <th>Facebook</th>
+                    <th>Avatar</th>
+                    <th>Thao tác</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    @foreach($users as $user)
+                <tr>
+                    <th>{{ $user->id }}</th>
+                    <th>{{ $user->name }}</th>
+                    <th>{{ $user->email }}</th>
+                    <th>{{ $user->like }}</th>
+                    <th>{{ $user->facebook }}</th>
+                    <th>
+                        <img src="{{ asset('storage/' . $user->avatar) }}" alt="{{ $user->name }}" style="width: 200px; height: 200px; object-fit: cover;">
+                    </th>
+                    <th>
+                        <a href="{{ route('user.readUser', ['id' => $user->id]) }}">View</a> |
+                        <a href="{{ route('user.updateUser', ['id' => $user->id]) }}">Edit</a> |
+                        <a href="{{ route('user.deleteUser', ['id' => $user->id]) }}">Delete</a>
+                    </th>
+                </tr>
+                @endforeach
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <footer class="navbar navbar-expand-sm bg-light justify-content-center my-footer">
+        <div class="container-fluid justify-content-center">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Lê Xuân Vũ - 23211TT4449</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</div>
+</div>
+@endsection
---
+++ my-lavarel/example-app/resources/views/crud_user/read.blade.php
@@ -0,0 +1,45 @@
+@extends('dashboard')
+
+@section('content')
+<div class="root">
+    <div class="background-list">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>UserName</th>
+                    <th>Email</th>
+                    <th>Like</th>
+                    <th>Facebook</th>
+                    <th>Avatar</th>
+                    <th>Thao tác</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{{$messi->id}}</td>
+                    <td>{{$messi->name}}</td>
+                    <td>{{$messi->email}}</td>
+                    <td>{{$messi->like}}</td>
+                    <td>{{$messi->facebook}}</td>
+                    <td>
+                        <img src="{{ asset('storage/' . $messi->avatar) }}" alt="{{ $messi->name }}" style="width: 200px; height: 200px; object-fit: cover;">
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <footer class="navbar navbar-expand-sm bg-light justify-content-center my-footer">
+        <div class="container-fluid justify-content-center">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Lê Xuân Vũ - 23211TT4449</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</div>
+</div>
+@endsection
